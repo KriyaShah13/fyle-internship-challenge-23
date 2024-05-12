79de24ecbd4b48c1ea2153b457ff425c85b3a1f5
Kriya Shah <shahkriya0508@gmail.com>
2024-05-12 10:44:57 +0530

    added navbar and fixed the gitub-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './navbar/navbar.component';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
@@ -13,6 +14,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
     AppComponent,
     GithubSearchComponent,
     GithubProfileComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
